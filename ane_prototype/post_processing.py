571baa2469b0233abc095d3636477cafa6155d06
epockonechny <pokonechnyy.ep@phystech.edu>
2018-06-21 01:04:42 +0300

    Globus finished. Good reg exp

--- ane_prototype/post_processing.py
+++ ane_prototype/post_processing.py
@@ -36,9 +36,20 @@ class PostProcessor:
 
         self.weight_patt1 = re.compile(r'\s+(?P<amount>\d{1,4}(?:[,.]\d{,3})?)'+\
                                        r'\s*(?P<unit>[гГgG]|грамм|Грамм|кг|Кг|КГ'+\
-                                       r'|kg|Kg|KG|мл|Мл|МЛ|[лЛlL]).?\s*$')  # group 1
+                                       r'|kg|Kg|KG|мл|Мл|МЛ|[лЛlL]|шт|Шт|штук|Штук).?\s*$')  # group 1
 
-        self.gram_patt2 = re.compile(r'(\d{1,4}(?:[,.]\d{,3})?)\s*[xXхХ\*×]\s*(\d{1,4}(?:[,.]\d{,3})?)') # iks and kha
+        self.weight_patt2 = re.compile(r'(?P<first>\d{1,4}(?:[,.]\d{,3})?)'+\
+                                       r'(?P<unit1>[гГgG]|грамм|Грамм|мл|Мл|МЛ)?\s*[xXхХ\*×]\s*'+\
+                                       r'(?P<second>\d{1,4}(?:[,.]\d{,3})?)\s*'+\
+                                       r'(?P<unit2>[гГgG]|грамм|Грамм|мл|Мл|МЛ)?.?\s*$') # iks and kha
+
+        self.weight_patt3 = re.compile(r'(?P<first>\d{1,4}(?:[,.]\d{,3})?)'+\
+                                       r'\s*[xXхХ\*×]\s*'+\
+                                       r'(?P<second>\d{1,4}(?:[,.]\d{,3})?)'+\
+                                       r'\s*[xXхХ\*×]\s*' +\
+                                       r'(?P<third>\d{1,4}(?:[,.]\d{,3})?)'+\
+                                       r'(?P<unit>[гГgG]|грамм|Грамм|мл|Мл|МЛ).?')
+
         self.kg_patt1 = re.compile(r'\s+(\d{1,4}(?:[,.]\d{,3})?)\s*(?:(?:кг|Кг|КГ)|(?:kg|Kg|KG)).?\s*$') # group 1
         # self.ml_patt1 = re.compile(r'\s+(\d{1,4}(?:[,.]\d{,3})?)\s*(?:(?:мл|Мл|МЛ)|(?:ml|Ml|ML)).?\s*$')
         # self.litre_patt1 = re.compile(r'\s+\d{1,4}(?:[,.]\d{,3})?\s*[лЛlL].?\s*$')
@@ -57,97 +68,61 @@ class PostProcessor:
         pass
 
     def get_coeff_by_amount_and_unit(self, amount, unit):
-
-
-    def extract_cost_pieces(self, row):
-        s_title = row['site_title']
-        s_unt = wspex(row['site_unit'])
-        s_cst = row['site_cost']
-
-        cur_cost = -6.6612345
-        if s_unt in self.package_per_units:
-            sr = self.piece_patt1.search(s_title)
-            if sr is not None:
-                pieces = tofloat(self.i_patt1.search(sr.group(0)).group(0))
-                cur_cost = s_cst * 10 / pieces
-            else:
-                print('**WARNING**: unknown unit (pieces):', s_unt)
-
-        cur_cost = round(cur_cost, 2)
-        return cur_cost
-
-    def extract_price_cost_pieces(self, price):
-        cost_column = []
-        for index, row in price.iterrows():
-            cur_cost = self.extract_cost_pieces(row)
-            cost_column.append(cur_cost)
-
-        price['unitcost'] = cost_column
-        return price
-
-    def extract_cost_kg(self, row):
-        s_title = row['site_title']
-        s_unt = wspex(row['site_unit'])
-        s_cst = row['site_cost']
-
-        cur_cost = -1.2345
-        if s_unt in self.kg_per_units:
-            cur_cost = s_cst
-        elif s_unt in (self.package_per_units + self.piece_per_units):
-            sr = self.gram_patt1.search(s_title)
-            if sr is not None:
-                grams = tofloat(self.f_patt1.search(sr.group(0)).group(0))
-                cur_cost = s_cst * 1000 / grams
-            else:
-                # кг case
-                sr = self.kg_patt1.search(s_title)
-                if sr is not None:
-                    kilograms = tofloat(self.f_patt1.search(sr.group(0)).group(0))
-                    cur_cost = s_cst / kilograms
-        else:
-            print('**WARNING**: unknown unit (kg)', s_unt)
-
-        cur_cost = round(cur_cost, 2)
-        return cur_cost
-
-    def extract_price_cost_kg(self, price):
-        cost_column = []
-        print('type =', type(price))
-        for index, row in price.iterrows():
-            cur_cost = self.extract_cost_kg(row)
-            cost_column.append(cur_cost)
-
-        price['unitcost'] = cost_column
-        return price
+        if unit in (self.gram_units + self.ml_units):
+            return 1000 / amount
+        elif unit in self.piece_units:
+            return 10 / amount
+        return 1. / amount
 
     def extract_cost_weight(self, row):
         s_title = row['site_title']
         s_unt = wspex(row['site_unit'])
         s_cst = row['site_cost']
 
-        cur_cost = -1.2345
-        if s_unt in self.litre_per_units:
+        cur_cost = -6666.66
+        if s_unt in (self.litre_per_units + self.kg_per_units):
             cur_cost = s_cst
         else:
-            sr = self.ml_patt1.search(s_title)
+            sr = self.weight_patt1.search(s_title)
             if sr is not None:
-                millilitres = tofloat(self.f_patt1(sr.group(0)).group(0))
-                cur_cost = s_cst * 1000 / millilitres
+                extracted_amount = tofloat(sr.group('amount'))
+                extracted_unit   = sr.group('unit')
+                coeff = self.get_coeff_by_amount_and_unit(extracted_amount, extracted_unit)
+                cur_cost = s_cst * coeff
             else:
-                # л case
-                sr = self.litre_patt1.search(s_title)
+                sr = self.weight_patt2.search(s_title)
                 if sr is not None:
-                    litres = tofloat(self.f_patt1(sr.group(0)).group(0))
-                    cur_cost = s_cst / litres
+                    extracted_first = tofloat(sr.group('first'))
+                    extracted_second = tofloat(sr.group('second'))
+                    if sr.group('unit1'):
+                        extracted_unit = sr.group('unit1')
+                    elif sr.group('unit2'):
+                        extracted_unit = sr.group('unit2')
+                    else:
+                        print('**WARNING**: unknown unit! (2 x 2) pattern:', sr.group(0))
+                    coeff = self.get_coeff_by_amount_and_unit(extracted_first * extracted_second, extracted_unit)
+                    cur_cost = s_cst * coeff
                 else:
-                    cur_cost = self.extract_cost_kg(row)
+                    sr = self.weight_patt3.search(s_title)
+                    if sr is not None:
+                        extracted_first = tofloat(sr.group('first'))
+                        extracted_second = tofloat(sr.group('second'))
+                        extracted_third = tofloat(sr.group('third'))
+                        extracted_unit = sr.group('unit')
+                        coeff = self.get_coeff_by_amount_and_unit(extracted_first * extracted_second *\
+                                                                  extracted_third, extracted_unit)
+                        cur_cost = s_cst * coeff
+                    else:
+                        print('**WARNING**: unknown pattern:', s_title)
+
+                        cur_cost = -111.11
         cur_cost = round(cur_cost, 2)
         return cur_cost
 
     def extract_price_cost_weight(self, price):
         cost_column = []
         for index, row in price.iterrows():
-            cur_cost = self.extract_cost_litres(row)
+            cur_cost = self.extract_cost_weight(row)
             cost_column.append(cur_cost)
 
         price['unitcost'] = cost_column
@@ -157,10 +132,9 @@ class PostProcessor:
         for product_id in price_dict.keys():
             prod_unit = SFB.STANDARD_FOOD_BASKET_INFO.loc[SFB.STANDARD_FOOD_BASKET_INFO['id'] ==
                                                                     product_id].iloc[0]['unit']
-            if prod_unit in (self.kg_units + self.gram_units + self.litre_units + self.ml_units):
+            print('processing product_id =', product_id)
+            if prod_unit in (self.kg_units + self.gram_units + self.litre_units + self.ml_units + self.tenpiece_units):
                 price_dict[product_id] = self.extract_price_cost_weight(price_dict[product_id])
-            elif prod_unit in ['10 шт', '10 шт.', '10шт', '10шт.']:
-                price_dict[product_id] = self.extract_price_cost_pieces(price_dict[product_id])
             else:
                 print('**WARNING**: unprocessed unit:', prod_unit)
                 pass
