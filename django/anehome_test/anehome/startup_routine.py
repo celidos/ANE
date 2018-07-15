e12522194e92d3536b4295f31f0433a0bd63ff4b
epockonechny <pokonechnyy.ep@phystech.edu>
2018-07-15 22:50:15 +0300

    small fixes

--- django/anehome_test/anehome/startup_routine.py
+++ django/anehome_test/anehome/startup_routine.py
@@ -1,23 +1,63 @@
 from anehome.settings.settings import BASE_DIR
 import re
 import os
+import datetime
+import pandas as pd
 
 ANE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(BASE_DIR)))
 SNAPSHOTS_DIR = os.path.join(ANE_DIR, 'snapshots')
 
-def scan_all_snapshots():
-    snapshot_folder_list = next(os.walk('.'))[1]
-    fresh_snapshot_name = None
+class FreshTablesHolder():
+    def __init__(self):
+        self.fresh_snapshot_name = None
+        self.fresh_snapshot_date = None
+        self.fresh_snapshot_dir = None
+        self.snapshot_folder_list = None
 
-    print(SNAPSHOTS_DIR)
+        self.gks_table = None
 
-    # for snapshot_folder_name in snapshot_folder_list:
-    #     re.match()
+    def scan_all_snapshots(self):
+
+        print(SNAPSHOTS_DIR)
+
+        self.snapshot_folder_list = next(os.walk(SNAPSHOTS_DIR))[1]
+
+        for snapshot_folder_name in self.snapshot_folder_list:
+            print(snapshot_folder_name)
+            sr = re.search('\Asnap_(?P<year>\d+)_(?P<month>\d+)_(?P<day>\d+)__(?P<hour>\d+)_(?P<minute>\d+)_(?P<second>\d+)$',
+                           snapshot_folder_name)
+            if sr:
+                snapdate = datetime.datetime(year=int(sr.group('year')),
+                                             month=int(sr.group('month')),
+                                             day=int(sr.group('day')),
+                                             hour=int(sr.group('hour')),
+                                             minute=int(sr.group('minute')),
+                                             second=int(sr.group('second')))
+                if (self.fresh_snapshot_date and self.fresh_snapshot_date < snapdate) or\
+                        (self.fresh_snapshot_date is None):
+                    self.fresh_snapshot_date = snapdate
+                    self.fresh_snapshot_name = snapshot_folder_name
+
+        print('fresh name =', self.fresh_snapshot_name)
+
+        if self.fresh_snapshot_name:
+            self.fresh_snapshot_dir = os.path.join(SNAPSHOTS_DIR, self.fresh_snapshot_name)
+
+            self.gks_table = pd.read_csv(os.path.join(self.fresh_snapshot_dir,
+                                                      'gks_acc.csv'))
+
+            print(self.gks_table)
 
 
-    pass
+
+
+        #     re.match()
+
+        pass
+
+fresh_table_holder = FreshTablesHolder()
 
 def startup_routine():
-    scan_all_snapshots()
+    fresh_table_holder.scan_all_snapshots()
 
     pass
--- django/anehome_test/anehome/views.py
+++ django/anehome_test/anehome/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 import django_tables2 as tables
 
+from anehome.startup_routine import fresh_table_holder
+
 class NameTable(tables.Table):
     name = tables.Column()
     age = tables.Column()
 
 def main(request):
-    table = NameTable([{'name': 'hui', 'age': 21}, {'name': 'pizda', 'age': 22}])
+    # table = NameTable([{'name': 'hui', 'age': 21}, {'name': 'pizda', 'age': 22}])
 
-    return render(request, 'index.html', {'people': table})
+
+
+    return render(request, 'index.html', {'mytable': fresh_table_holder.gks_table.to_html()})
